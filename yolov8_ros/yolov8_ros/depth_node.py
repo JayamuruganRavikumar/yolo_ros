8a4844734b54f68540c4379bab6af745c60b95e4
Jay <jayamurugancbe@gmail.com>
2024-08-06 18:07:58 +0200

    depth node

--- yolov8_ros/yolov8_ros/depth_node.py
+++ yolov8_ros/yolov8_ros/depth_node.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from typing import List, Tuple
+import signal
 
 import rclpy
 from rclpy.qos import QoSProfile
@@ -21,6 +22,7 @@ from sensor_msgs.msg import CameraInfo, Image
 from geometry_msgs.msg import TransformStamped
 from yolov8_msgs.msg import Detection
 from yolov8_msgs.msg import DetectionArray
+from yolov8_msgs.msg import BoundingBox3D
 
 class DepthNode(LifecycleNode):
 
@@ -56,10 +58,19 @@ class DepthNode(LifecycleNode):
             durability=QoSDurabilityPolicy.VOLATILE,
             depth=1
         )
+        dinfo_reliability = self.get_parameter(
+            "depth_info_reliability").get_parameter_value().integer_value
+
+        self.depth_info_qos_profile = QoSProfile(
+            reliability=dinfo_reliability,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            durability=QoSDurabilityPolicy.VOLATILE,
+            depth=1
+        )
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
         # pubs
-        self._pub = self.create_publisher(DetectionArray, "detections_position", 10)
+        self._pub = self.create_publisher(DetectionArray, "detections3D", 10)
 
         return TransitionCallbackReturn.SUCCESS
 
@@ -67,29 +78,39 @@ class DepthNode(LifecycleNode):
         self.get_logger().info(f"Activating {self.get_name()}")
 
         self.depth_sub = message_filters.Subscriber(
-            self, Image, "depth_image",
+            self, Image, "/yolo/depth_to_rgb/image_raw",
             qos_profile=self.depth_image_qos_profile)
+        self.get_logger().info(f"depth {self.get_name()}")
         self.depth_info_sub = message_filters.Subscriber(
-            self, CameraInfo, "depth_info",
+            self, CameraInfo, "/yolo/depth/camera_info",
             qos_profile=self.depth_info_qos_profile)
+        self.get_logger().info(f"cam info {self.get_name()}")
         self.detections_sub = message_filters.Subscriber(
-            self, DetectionArray, "detections")
+            self, DetectionArray, "/yolo/detections",)
 
         self._synchronizer = message_filters.ApproximateTimeSynchronizer(
             (self.depth_sub, self.depth_info_sub, self.detections_sub), 10, 0.5)
+        self.get_logger().info(f"sync {self.get_name()}")
         self._synchronizer.registerCallback(self.on_detections)
+        self.get_logger().info(f"callback {self.get_name()}")
 
         return TransitionCallbackReturn.SUCCESS
 
     def on_deactivate(self, state: LifecycleState) -> TransitionCallbackReturn:
         self.get_logger().info(f"Deactivating {self.get_name()}")
 
-        self._synchronizer.clear()
+        #self._synchronizer.clear()
         self.depth_sub.destroy()
         self.depth_info_sub.destroy()
         self.detections_sub.destroy()
 
         return TransitionCallbackReturn.SUCCESS
+
+    def on_unconfigured(self, state: LifecycleState) -> TransitionCallbackReturn:
+        self.get_logger().info(f"Unconfiguring {self.get_name()}")
+
+        return TransitionCallbackReturn.SUCCESS
+
 
     def on_cleanup(self, state: LifecycleState) -> TransitionCallbackReturn:
         self.get_logger().info(f"Cleaning up {self.get_name()}")
@@ -99,6 +120,66 @@ class DepthNode(LifecycleNode):
 
         return TransitionCallbackReturn.SUCCESS
 
-    def on_detections(self, depth_image: Image, depth_info: CameraInfo, detections: DetectionArray):
+    def on_detections(self, depth_msg: Image, depth_info_msg: CameraInfo, detections_msg: DetectionArray):
+
+        new_detections_msg = DetectionArray()
+        new_detections_msg.header = detections_msg.header
+        new_detections_msg.detections = self.process_detections(
+            depth_msg, depth_info_msg, detections_msg)
+        self._pub.publish(new_detections_msg)
+
+    def process_detections(self, depth_msg: Image, depth_info_msg: CameraInfo, detections_msg: DetectionArray) -> List[Detection]:
+
+        #Check if there are detections
+        if not detections_msg.detections:
+            return []
+        new_detections = []
+        depth_image = self.cv_bridge.imgmsg_to_cv2(depth_msg, "16UC1")
+
+        for detection in detections_msg.detections:
+            bbox3d = self.image_to_world(depth_image, depth_info_msg, detection)
+            
+            if bbox3d is not None:
+                new_detections.append(detection)
+                new_detections[-1].bbox3d = bbox3d
+
+
+        return new_detections
+
+    def image_to_world(self, depth_image: np.ndarray, depth_info: CameraInfo, detection: Detection) -> BoundingBox3D:
+
+        center_x = int(detection.bbox.center.position.x)
+        center_y = int(detection.bbox.center.position.y)
+        z = depth_image[center_y, center_x] 
+
+        #Get camera intrinsic parameters
+        # Camera matrix K = [fx 0 cx]
+        #                   [0 fy cy]
+        #                   [0 0  1 ]
+
+        # project from image to world space
+        k = depth_info.k
+        cx, cy, fx, fy = k[2], k[5], k[0], k[4]
+        x = z * (center_x - cx) / fx
+        y = z * (center_y - cy) / fy
+
+        # Update detections with depth information
+
+        msg = BoundingBox3D()
+        msg.center.position.x = x
+        msg.center.position.y = y
+        msg.center.position.z = float(z)
+
+        return msg
+
+def main():
+    rclpy.init()
+    node = DepthNode()
+
+    node.trigger_configure()
+    node.trigger_activate()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
